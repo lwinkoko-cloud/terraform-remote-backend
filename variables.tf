d33a1be7452918ca44e88db3670706cc036ff00a
lwinkoko95 <lwinkoko.cu@gmail.com>
2024-07-28 20:19:34 +0630

    Added terraform s3 and dynamodb config

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
 variable "aws_region" {
-  type    = string
+  type = string
   default = "us-east-1"
+  
+}
 
+variable "s3_bucket_name" {
+  type = string
+  default = my_s3_bucket
+  
+}
+
+variable "dynamodb_name" {
+  description = "dynamodb name"
+  type = string
+  default = "my_dynamodb_table" 
+}
+
+variable "dynamodb_billing_mode" {
+  description = "dynamodb_billing mode"
+  type = string
+  default = "AY_PER_REQUEST"
+  
 }
